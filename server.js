cd76d4e6f8fcf094e283688d180248ff41de8e12
Vincent Dang Dev <vincentdang004@gmail.com>
2024-01-12 08:45:43 +0700

    video 5

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const http = require('http');
+const express = require('express');
+const app = express();//muon su dung het chuc nang cua ham 
+//express thi can goi ham express.
+const port = 2004;
 
-const server = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello world! </h3>');
-    res.write('<h2>from ERIC & HOI DAN IT </h2>');
-    res.end();
+app.get('/', (req, res) => {
+    res.send('Hello World vs Vincent Dang Dev!')
 })
-
-server.listen(3000, 'localhost', () => {
-    console.log('Node.JS server is running on port: 3000');
+app.get('/about', (req, res) => {
+    res.send(`I'm Eric!`)
 })
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})//
